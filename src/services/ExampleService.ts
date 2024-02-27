4d7552ac03ab389c39ccba6c3bf721f00cd0088e
dagtekin00 <138658972+Dagtekin03@users.noreply.github.com>
2024-02-27 17:10:08 +0300

    example model changed

--- src/services/ExampleService.ts
+++ src/services/ExampleService.ts
@@ -1,22 +1,22 @@
 import { ApiResult } from "@/models/ApiResult"
 import { BaseService } from "./Common/BaseService"
 import type { AxiosResponse } from "axios"
-import { ExampleReqModel, ExampleResModel } from "../models/entities/ExampleModels"
+import { ExampleModel } from "../models/entities/ExampleModels"
 
 export class GroupService extends BaseService {
     constructor() {
         super("Group")
     }
-    Get(id: string): Promise<AxiosResponse<ApiResult<ExampleResModel>>> {
-        return this.client.Get<ApiResult<ExampleResModel>>(`${this.baseUrl}/Get?id=${id}`)
+    Get(id: string): Promise<AxiosResponse<ApiResult<ExampleModel>>> {
+        return this.client.Get<ApiResult<ExampleModel>>(`${this.baseUrl}/Get?id=${id}`)
     }
-    Post(payload: ExampleReqModel): Promise<AxiosResponse<ApiResult<ExampleResModel>>> {
-        return this.client.Post<ApiResult<ExampleResModel>>(`${this.baseUrl}/EditGroup`, payload)
+    Post(payload: ExampleModel): Promise<AxiosResponse<ApiResult<ExampleModel>>> {
+        return this.client.Post<ApiResult<ExampleModel>>(`${this.baseUrl}/EditGroup`, payload)
     }
-    Delete(id: number): Promise<AxiosResponse<ApiResult<ExampleResModel>>> {
-        return this.client.Delete<ApiResult<ExampleResModel>>(`${this.baseUrl}/${id}`, null)
+    Delete(id: number): Promise<AxiosResponse<ApiResult<ExampleModel>>> {
+        return this.client.Delete<ApiResult<ExampleModel>>(`${this.baseUrl}/${id}`, null)
     }
-    Update(id: number, payload: ExampleReqModel): Promise<AxiosResponse<ApiResult<ExampleResModel>>> {
-        return this.client.Put<ApiResult<ExampleResModel>>(`${this.baseUrl}/${id}`, payload)
+    Update(id: number, payload: ExampleModel): Promise<AxiosResponse<ApiResult<ExampleModel>>> {
+        return this.client.Put<ApiResult<ExampleModel>>(`${this.baseUrl}/${id}`, payload)
     }
 }
